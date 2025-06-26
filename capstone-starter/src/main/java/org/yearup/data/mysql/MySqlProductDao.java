1352ff87eb86886da0b3684513161bb71e3c7851
James Bosley <jamesbosley123@gmail.com>
2025-06-26 14:03:55 -0500

    Bug 2

--- capstone-starter/src/main/java/org/yearup/data/mysql/MySqlProductDao.java
+++ capstone-starter/src/main/java/org/yearup/data/mysql/MySqlProductDao.java
@@ -41,8 +41,10 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
             statement.setInt(2, categoryId);
             statement.setBigDecimal(3, minPrice);
             statement.setBigDecimal(4, minPrice);
-            statement.setString(5, color);
-            statement.setString(6, color);
+            statement.setBigDecimal(5, maxPrice);
+            statement.setBigDecimal(6, maxPrice);
+            statement.setString(7, color);
+            statement.setString(8, color);
 
             ResultSet row = statement.executeQuery();
 
